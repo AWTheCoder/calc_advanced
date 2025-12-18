509ea1827c151aa8f26ffa29203ccb18c3fee6bf
Jamz@theFunkchion <165467865+Bruddanem67@users.noreply.github.com>
2025-12-18 09:34:28 +0000

    Update Arith.py
    I cooked with this one

--- Arith.py
+++ Arith.py
@@ -38,3 +38,47 @@ elif choice == "/":
 
 else:
     print("Invalid operation")
+
+def perimeter():
+    return (2*width) + (2*length)
+print("""========================
+╔═╗┌─┐┬─┐┬┌┬┐┌─┐┌┬┐┌─┐┬─┐
+╠═╝├┤ ├┬┘││││├┤  │ ├┤ ├┬┘
+╩  └─┘┴└─┴┴ ┴└─┘ ┴ └─┘┴└─
+========================""")
+while True:
+    try:
+        width = float(input("Enter the width of your rectangle/square: "))
+        length = float(input("Enter the length of your rectangle/square: "))
+        if width < 1 or length < 1:
+            print("Must be positive")
+            continue
+        peri = perimeter()
+        print("The perimeter is", peri, "\n")
+        break
+    except ValueError:
+        print("Enter a valid number")
+        continue
+def area_and_circumference(diameter):
+    radius = diameter / 2
+    circumference = diameter * 3.14
+    area_calc = (radius * radius * 3.14)
+    return area_calc, circumference
+print("""=============================================================
+┌─┐┬─┐┌─┐┌─┐  ┌─┐┌┐┌┌┬┐  ┌─┐┬┬─┐┌─┐┬ ┬┌┬┐┌─┐┌─┐┬─┐┌─┐┌┐┌┌─┐┌─┐
+├─┤├┬┘├┤ ├─┤  ├─┤│││ ││  │  │├┬┘│  │ ││││├┤ ├┤ ├┬┘├┤ ││││  ├┤ 
+┴ ┴┴└─└─┘┴ ┴  ┴ ┴┘└┘─┴┘  └─┘┴┴└─└─┘└─┘┴ ┴└  └─┘┴└─└─┘┘└┘└─┘└─┘
+=============================================================""")
+while True:
+    try:
+        diameter = float(input("Enter the diameter of the circle: "))
+        if diameter <= 0:
+            print("Must be positive")
+            continue
+        area_calc, circ_calc = area_and_circumference(diameter)
+        print("The area of your circle is", area_calc)
+        print("The circumference of your circle is", circ_calc)
+        break
+    except ValueError:
+        print("Enter a valid number")
+        continue
